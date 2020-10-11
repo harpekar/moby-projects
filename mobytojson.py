a023537e2f07260517dbfcae8620caf4423bf414
Karen <karenl.harper88@gmail.com>
2020-10-11 16:05:35 -0700

    Fixed json for words with more than one part of speech

--- mobytojson.py
+++ mobytojson.py
@@ -10,7 +10,9 @@ with open("mobypos.txt", 'r') as m:
     for line in m:
         line = line.rstrip() #Remove newline characters
         (word, pos) = re.split(r'\\', line)
-        words[pos] = [word] if pos not in words.keys() else words[pos] + [word]
+
+        for part in pos: #Place word in every part of speech it has
+            words[part] = [word] if part not in words.keys() else words[part] + [word]
 
 with open("mobywords.json", "w") as outfile:
     json.dump(words, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
