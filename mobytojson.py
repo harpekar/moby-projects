42bd2668cfdcbb043f47cf1f57387db32a16c926
Karen <karenl.harper88@gmail.com>
2020-10-11 15:53:25 -0700

    Json file is pos first, word array second instead of word-pos pairs

--- mobytojson.py
+++ mobytojson.py
@@ -10,7 +10,7 @@ with open("mobypos.txt", 'r') as m:
     for line in m:
         line = line.rstrip() #Remove newline characters
         (word, pos) = re.split(r'\\', line)
-        words[word] = pos
+        words[pos] = [word] if pos not in words.keys() else words[pos] + [word]
 
 with open("mobywords.json", "w") as outfile:
     json.dump(words, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
